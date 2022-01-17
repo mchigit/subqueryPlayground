6ff59d4988181db64f1ac0673932d3be44a2167d
michael <michael.chi1997@gmail.com>
2022-01-16 21:54:38 -0500

    fix

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,8 +1,6 @@
 import { Transaction } from "../types";
-import {
-  MoonbeamEvent,
-  MoonbeamCall,
-} from "@subql/contract-processors/dist/moonbeam";
+import { MoonbeamEvent } from "@subql/contract-processors/dist/moonbeam";
+import { BigNumber } from "ethers";
 
 // export async function handleBlock(block: SubstrateBlock): Promise<void> {
 //     //Create a new starterEntity with ID using block hash
@@ -11,8 +9,15 @@ import {
 //     record.field1 = block.block.header.number.toNumber();
 //     await record.save();
 // }
+type TransferEventArgs = [string, string, BigNumber] & {
+  from: string;
+  to: string;
+  value: BigNumber;
+};
 
-export async function handleMoonbeamEvent(event: MoonbeamEvent): Promise<void> {
+export async function handleMoonbeamEvent(
+  event: MoonbeamEvent<TransferEventArgs>
+): Promise<void> {
   logger.info("got event");
   logger.info(event);
 
